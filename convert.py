60c982eff7a37309532b8f2c7b95c7eaa0640ef7
Alex Standke <xanderstrike@gmail.com>
2017-03-07 06:04:48 +0000

    backgrounding

---
+++ convert.py
@@ -0,0 +1,21 @@
+import sys
+import subprocess
+import time
+
+filename = sys.argv[1] + ".h264"
+
+command = "MP4Box -fps 25 -add " + filename + " tmp.mp4"
+subprocess.call(command, shell=True)
+
+duration = subprocess.check_output("avprobe -show_format tmp.mp4 2>&1 | sed -n '/duration/s/.*=//p'", shell=True)
+duration = int(float(duration))
+
+start = duration - 10
+
+stop_time = int(time.time())
+command = "MP4Box -add tmp.mp4 -splitx " + str(start) + ":" + str(duration) + " " + str(stop_time) + ".mp4"
+subprocess.call(command, shell=True)
+
+#subprocess.call("omxplayer " + str(stop_time) + ".mp4", shell=True)
+
+subprocess.call("rm tmp.mp4 " + filename, shell=True)
--- replay.py
+++ replay.py
@@ -8,11 +8,14 @@ led = Squid(18, 23, 24)
 button = Button(25)
 camera = picamera.PiCamera(resolution='720p', framerate=60)
 
+filename = 0
 def reset_camera():
+  global filename
+  filename += 1
   subprocess.call("rm tmp.mp4 video.h264", shell=True)
   camera.start_preview(alpha=128)
   sleep(1)
-  camera.start_recording('video.h264')
+  camera.start_recording(str(filename) + '.h264')
 
 reset_camera()
 
@@ -24,20 +27,7 @@ while(True):
     camera.stop_recording()
     camera.stop_preview()
 
-    command = "MP4Box -fps 25 -add video.h264 tmp.mp4"
-    subprocess.call(command, shell=True)
-
-    duration = subprocess.check_output("avprobe -show_format tmp.mp4 2>&1 | sed -n '/duration/s/.*=//p'", shell=True)
-    duration = int(float(duration))
- 
-    start = duration - 10
-
-    stop_time = int(time.time())
-    command = "MP4Box -add tmp.mp4 -splitx " + str(start) + ":" + str(duration) + " " + str(stop_time) + ".mp4"
-    subprocess.call(command, shell=True)
-
-    led.set_color(GREEN)
-    subprocess.call("omxplayer " + str(stop_time) + ".mp4", shell=True)
+    subprocess.call("python convert.py " + str(filename) + " &", shell=True)
     
     reset_camera()
   else:
