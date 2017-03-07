85be5d3db76d07009db68d0ce2880b8a4071ffaa
Alex Standke <xanderstrike@gmail.com>
2017-03-07 04:49:24 +0000

    asdf

--- replay.py
+++ replay.py
@@ -6,7 +6,7 @@ import subprocess
 
 led = Squid(18, 23, 24)
 button = Button(25)
-camera = picamera.PiCamera(resolution=(1024,576), framerate=30)
+camera = picamera.PiCamera(resolution=(1024,576), framerate=60)
 
 def reset_camera():
   camera.start_preview(alpha=128)
@@ -14,25 +14,29 @@ def reset_camera():
   camera.start_recording('video.h264')
 
 reset_camera()
-start_time = int(time.time())
 
 while(True):
   if button.is_pressed():
     led.set_color(RED)
-    sleep(2)
-    stop_time = int(time.time())
+    sleep(3)
     camera.stop_recording()
-    end = stop_time - start_time
-    start = end - 10
-    command = "MP4Box -fps 25 -add video.h264 " + str(stop_time) + ".mp4"
-    print command
-    subprocess.call(command, shell=True)
     camera.stop_preview()
+
+    command = "MP4Box -fps 25 -add video.h264 tmp.mp4"
+    subprocess.call(command, shell=True)
+
+    duration = int(subprocess.call("avprobe -show_format tmp.mp4 2>&1 | sed -n '/duration/s/.*=//p'", shell=True))
+ 
+    start = duration - 10
+
+    stop_time = int(time.time())
+    command = "MP4Box -add tmp.mp4 -splitx " + str(start) + ":" + str(duration) + " " + str(stop_time) + ".mp4"
+    subprocess.call(command, shell=True)
+
     led.set_color(GREEN)
     subprocess.call("omxplayer " + str(stop_time) + ".mp4", shell=True)
     
     reset_camera()
-    start_time = int(time.time())
   else:
     led.set_color(BLUE)
     sleep(0.1)
