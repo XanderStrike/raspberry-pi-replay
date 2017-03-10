319ca1166f5d98372f00d3e045a6314d365da642
Alex Standke <xanderstrike@gmail.com>
2017-03-10 01:53:57 +0000

    maybe slack notification

--- endgame.py
+++ endgame.py
@@ -2,6 +2,9 @@ import sys
 import subprocess
 import glob, os
 
+import json
+import requests
+
 print "=== ENDING GAME ==="
 
 command = "MP4Box"
@@ -10,4 +13,14 @@ for file in glob.glob("*.mp4"):
 
 subprocess.call(command + " out.mp4", shell=True)
 
-subprocess.call("/usr/local/bin/youtube-upload  --playlist \"Foosball Replays\" --client-secrets=/home/pi/replay/client_id.json --title=\"Foosball Replay\" out.mp4", shell=True)
+subprocess.call("/usr/local/bin/youtube-upload  --playlist \"Foosball Replays\" --client-secrets=/home/pi/replay/client_id.json --title=\"Foosball Replay\" out.mp4 > youtube 2>&1", shell=True)
+
+url = open('youtube', 'r').read().splitlines()[-3]
+
+webhook_url = "https://hooks.slack.com/services/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"
+slack_data = {'text': url, 'username': "Replay Bot", 'icon_emoji': "movie_camera"}
+
+response = requests.post(
+    webhook_url, data=json.dumps(slack_data),
+    headers={'Content-Type': 'application/json'}
+)
